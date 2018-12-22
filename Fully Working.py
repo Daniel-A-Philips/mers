e072757db303bdfd67291a17549e5200a5821609
philipsddd <46075925+philipsddd@users.noreply.github.com>
2018-12-22 23:59:49 +0200

    Add files via upload

--- Fully Working.py
+++ Fully Working.py
@@ -21,6 +21,6 @@ def prime(number):
 for i in range(1, maxnum, 2):  # generate up to M20, found in 1961
     if prime(i) and mersenne(i):
         b = ((2 ** i) - 1)
-        print("✓   2^"+str(i)+" -1")
+        print("2^"+str(i)+" -1")
 print("done")
 
